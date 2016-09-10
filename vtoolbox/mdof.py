438fc2161997a1493dfb3accd2331f6874e3913f
raphaeltimbo <raphaelts@gmail.com>
2016-09-10 12:28:01 -0300

    Added the function normalize. This function will be used to normalize the left eigenvectors.

--- vtoolbox/mdof.py
+++ vtoolbox/mdof.py
@@ -61,6 +61,56 @@ def eigen(A, B=None):
     return evalues[idx], evectors[:, idx]
 
 
+def normalize(X, Y):
+    """
+    This function is used to normalize vectors of the matrix
+    Y with respect to X so that Y.T @ X = I (identity).
+    This is used to normalize the matrix with the left eigenvectors.
+
+    Parameters
+    ----------
+    X: array
+        A complex or real matrix
+    Y: array
+        A complex or real matrix to be normalized
+
+    Returns
+    ----------
+    Yn: array
+        Normalized matrix
+
+    Examples:
+    >>> X = sp.array([[ 0.84+0.j  ,  0.14-0.j  ,  0.84-0.j  ,  0.14+0.j  ],
+    ...               [ 0.01-0.3j ,  0.00+0.15j,  0.01+0.3j ,  0.00-0.15j],
+    ...               [-0.09+0.42j, -0.01+0.65j, -0.09-0.42j, -0.01-0.65j],
+    ...               [ 0.15+0.04j, -0.74+0.j  ,  0.15-0.04j, -0.74-0.j  ]])
+    >>> Y = sp.array([[-0.03-0.41j,  0.04+0.1j , -0.03+0.41j,  0.04-0.1j ],
+    ...            [ 0.88+0.j  ,  0.68+0.j  ,  0.88-0.j  ,  0.68-0.j  ],
+    ...            [-0.21-0.j  ,  0.47+0.05j, -0.21+0.j  ,  0.47-0.05j],
+    ...            [ 0.00-0.08j,  0.05-0.54j,  0.00+0.08j,  0.05+0.54j]])
+    >>> Yn = normalize(X, Y)
+    >>> Yn
+    array([[ 0.57822773 -4.69882840e-02j,  0.11696967 -5.85231773e-02j,
+             0.57822773 +4.69882840e-02j,  0.11696967 +5.85231773e-02j],
+           [ 0.00998912 +1.24180506e+00j, -0.06879320 -8.22911025e-01j,
+             0.00998912 -1.24180506e+00j, -0.06879320 +8.22911025e-01j],
+           [-0.00238377 -2.96339843e-01j,  0.01295993 -5.73835061e-01j,
+            -0.00238377 +2.96339843e-01j,  0.01295993 +5.73835061e-01j],
+           [ 0.11289137 -9.08101611e-04j, -0.65854649 -5.87827297e-03j,
+             0.11289137 +9.08101611e-04j, -0.65854649 +5.87827297e-03j]])
+    """
+
+    Yn = sp.zeros_like(X)
+    YTX = Y.T @ X # normalize y so that Y.T @ X will return I
+    factors = [1/a for a in sp.diag(YTX)]
+    # multiply each column in y by a factor in 'factors'
+    for col in enumerate(Y.T):
+        Yn[col[0]] = col[1]*factors[col[0]]
+    Yn = Yn.T
+
+    return Yn
+
+
 def modes_system_undamped(M, K):
     """
     This function will return the natural frequencies (w),
