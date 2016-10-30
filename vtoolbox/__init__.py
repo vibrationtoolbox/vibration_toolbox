3e41a04e4fda453396ece6f6f90fcdb6ebe35ff3
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-10-30 09:39:09 -0400

    atom cleanups

--- vtoolbox/__init__.py
+++ vtoolbox/__init__.py
@@ -5,22 +5,21 @@ This will not be a massively comprehensive set of tools,
 so  `__all__` should contain any submodules should also be imported
 when a user executes "from vtoolbox import *"
 
-As there are no submodules, this is a placeholder in case such ever happens. 
+As there are no submodules, this is a placeholder in case such ever happens.
 
 __all__ = ['directory']
 """
 
 
 """
-With this, `from vtoolbox import *` will import 
+With this, `from vtoolbox import *` will import
 all toolbox functions in the name space
 
 `import vtoolbox as vtb` will keep them tucked behind `vtb`
 
-`import vtoolbox.sdof as sdof` will tuck the sdoc functions in the `sdof` box. 
+`import vtoolbox.sdof as sdof` will tuck the sdoc functions in the `sdof` box.
 
 """
 from .sdof import *
 from .mdof import *
 from .ema import *
-
