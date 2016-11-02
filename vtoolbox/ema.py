f15641c6f1389d31640cdf0c76dd317b779acc9a
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-11-02 08:53:22 -0400

    is it not loading the data

--- vtoolbox/ema.py
+++ vtoolbox/ema.py
@@ -35,7 +35,10 @@ def frf(x, f, dt):
     >>> # First we need to load the sampled data which in a .mat file
     >>> import vtoolbox as vt
     >>> import scipy.io as sio
+    >>> import os
+    >>> print(os.getcwd())
     >>> data = sio.loadmat('vtoolbox/data/frf_data1.mat')
+    >>> print(data)
     >>> # Data is imported as arrays. We need to modify then to fit our function
     >>> x = data['x']
     >>> x = x.reshape(len(x))
