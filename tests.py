b5068811ec24d83b3195bdb2898390d52b61d3e9
Joseph C. Slater <joseph.c.slater@gmail.com>
2016-11-19 09:42:15 -0500

    draft concepts to make nose tests work on travis

---
+++ tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import matplotlib
+import nose
+matplotlib.use('agg')
+
+nose.main(argv=' --with-doctest --doctest-tests --doctest-options=+ELLIPSIS,+NORMALIZE_WHITESPACE)
+
+#  - nosetests --with-doctest --doctest-tests --doctest-options=+ELLIPSIS,+NORMALIZE_WHITESPACE
