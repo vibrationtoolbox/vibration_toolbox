b5c7022ca76dc0180c24bfe300709c4cb1220cbd
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-04-09 21:45:03 -0400

    Generated gh-pages for commit be1edfa Author: Joseph C. Slater <joseph.c.slater@gmail.com>
        Keep examples in sync

--- index-4.py
+++ index-4.py
@@ -1,7 +1,3 @@
 import matplotlib.pyplot as plt
 import vibration_toolbox as vtb
-omega_n, x, U = vtb.euler_beam_modes(n=1)
-plt.plot(x,U)
-plt.xlabel('x (m)')
-plt.ylabel('Displacement (m)')
-plt.title('Mode 1')
+omega_n, x, U = vtb.euler_beam_frf()
