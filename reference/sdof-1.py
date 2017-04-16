de4921f3f0658104488d3d0a40d4a603432914df
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-04-16 18:57:01 -0400

    Generated gh-pages for commit 813cd2a Author: Joseph C. Slater <joseph.c.slater@gmail.com>
        docs updated

--- reference/sdof-1.py
+++ reference/sdof-1.py
@@ -8,7 +8,11 @@ vtb.free_response()[1][:5] # get the first five values of x
 # [ 0.98273317]])
 t, x, *_ = vtb.free_response() # *_ ignores all other returns
 plt.plot(t,x)
+# [<matplotlib.lines.Line2D object at ...>]
 plt.xlabel('Time (sec)')
+# <matplotlib.text.Text object at ...>
 plt.ylabel('Displacement (m)')
+# <matplotlib.text.Text object at ...>
 plt.title('Displacement versus time')
+# <matplotlib.text.Text object at ...>
 plt.grid('on')
