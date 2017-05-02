1f2aa7f631fd154c16825003b6d3c4a1c76ba068
raphaeltimbo <raphaelts@gmail.com>
2017-05-01 21:28:25 -0300

    Checking if travis will pass with this.

--- tests.py
+++ tests.py
@@ -2,7 +2,9 @@
 
 import matplotlib
 import nose
+import numpy as np
 matplotlib.use('agg')
+np.set_printoptions(precision=2, suppress=True)
 
 nose.main(argv=['fake', 'vibration_toolbox','--with-doctest','--doctest-tests','--doctest-options=+ELLIPSIS,+NORMALIZE_WHITESPACE'])
 
