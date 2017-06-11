bar code didn't run, and gave wrong results. fixed now

vibration_toolbox/continuous_systems.py
@@ -333,7 +333,6 @@ def uniform_bar_modes(n=10, bctype=3, npoints=2001,
     npoints: int
         number of points for returned mode shape array
 
-
     Returns
     -------
     omega_n: numpy array
@@ -342,7 +341,6 @@ def uniform_bar_modes(n=10, bctype=3, npoints=2001,
         x coordinate
     U: numpy array
         mass normalized mode shape
-
 
     Examples
     --------
@@ -370,12 +368,12 @@ def uniform_bar_modes(n=10, bctype=3, npoints=2001,
         ln = len(n)
 
     # len=[0:(1/(npoints-1)):1]';  %Normalized length of the bar
-    len = np.linspace(0, 1, npoints)
-    x = len * L
+    x_normed = np.linspace(0, 1, npoints, endpoint = True)
+    x = x_normed * L
     # Determine natural frequencies and mode shapes depending on the
     # boundary condition.
     # Mass simplification. The following was arange_(1,length_(n)).reshape(-1)
-    mode_num_range = np.arange(0, ln)
+    mode_num_range = np.arange(1, ln)
     w = np.empty(ln)
     U = np.empty([npoints, ln])
 
@@ -383,18 +381,17 @@ def uniform_bar_modes(n=10, bctype=3, npoints=2001,
         desc = 'Free-Free '
         for i in mode_num_range:
             w[i] = i * np.pi * np.sqrt(E/rho) / L
-            U[:, i] = np.cos(i * np.pi * len / L)
+            U[:, i] = np.cos(i * np.pi * x_normed)
     elif bctype == 2:
         desc = 'Fixed-Free '
         for i in mode_num_range:
             w[i] = (2*i-1) * np.pi * np.sqrt(E/rho) / (2 * L)
-            U[:, i] = np.sin((2*i-1) * np.pi * len / (2 * L))
+            U[:, i] = np.sin((2*i-1) * np.pi * x_normed / 2)
     elif bctype == 3:
         desc = 'Fixed-Fixed '
         for i in mode_num_range:
             w[i] = i * np.pi * np.sqrt(E/rho) / L
-            U[:, i] = np.sin(i * np.pi * len / (2 * L))
-
+            U[:, i] = np.sin((i) * np.pi * x_normed)
     # Mass Normalization of mode shapes
     for i in mode_num_range:
         U[:, i] = U[:, i] / np.sqrt(np.dot(U[:, i], U[:, i]) * rho * L)
@@ -402,11 +399,11 @@ def uniform_bar_modes(n=10, bctype=3, npoints=2001,
     omega_n = w
     return omega_n, x, U
 
-
+"""
 def ebf(xin, xout, fmin, fmax, zeta):
     _, _ = uniform_bar_frf(xin, xout, fmin, fmax, zeta)
     return
-
+"""
 
 if __name__ == "__main__":
     import doctest