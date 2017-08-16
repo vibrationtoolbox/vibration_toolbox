a90e173585cdd5142cbffd33cf02153f96463cdc
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-08-16 15:17:43 -0400

    time for a new release with new "stuff"
    fixed MDOF examples and added experimental modal analysis

--- build/lib/vibration_toolbox/mdof.py
+++ build/lib/vibration_toolbox/mdof.py
@@ -6,15 +6,10 @@ import matplotlib as mpl
 mpl.rcParams['lines.linewidth'] = 2
 mpl.rcParams['figure.figsize'] = (10, 6)
 
-__all__ = ["modes_system",
-           "modes_system_undamped",
-           "response_system",
-           "response_system_undamped"]
 
+def _eigen(A, B=None):
+    """Sorted eigenvector/eigenvalue pairs in order of increasing eigenvalues.
 
-def eigen(A, B=None):
-    """
-    This function is used to sort eigenvalues and eigenvectors
     e.g. for a given system linalg.eig will return eingenvalues as:
     (array([ 0. +89.4j,  0. -89.4j,  0. +89.4j,  0. -89.4j,  0.+983.2j,  0.-983.2j,  0. +40.7j,  0. -40.7j])
     This function will sort this eigenvalues as:
@@ -39,10 +34,11 @@ def eigen(A, B=None):
 
     Examples
     --------
+    >>> import vibration_toolbox as vtb
     >>> L = np.array([[2, -1, 0],
     ...               [-4, 8, -4],
     ...               [0, -4, 4]])
-    >>> lam, P = eigen(L)
+    >>> lam, P = vtb.mdof._eigen(L)
     >>> lam
     array([  0.56+0.j,   2.63+0.j,  10.81+0.j])
 
@@ -69,7 +65,7 @@ def eigen(A, B=None):
     return evalues[idx], evectors[:, idx]
 
 
-def normalize(X, Y):
+def _normalize(X, Y):
     """
     This function is used to normalize vectors of the matrix
     Y with respect to X so that Y.T @ X = I (identity).
@@ -97,7 +93,7 @@ def normalize(X, Y):
     ...            [ 0.88+0.j  ,  0.68+0.j  ,  0.88-0.j  ,  0.68-0.j  ],
     ...            [-0.21-0.j  ,  0.47+0.05j, -0.21+0.j  ,  0.47-0.05j],
     ...            [ 0.00-0.08j,  0.05-0.54j,  0.00+0.08j,  0.05+0.54j]])
-    >>> Yn = normalize(X, Y)
+    >>> Yn = _normalize(X, Y)
     >>> Yn
     array([[ 0.58-0.05j,  0.12-0.06j,  0.58+0.05j,  0.12+0.06j],
            [ 0.01+1.24j, -0.07-0.82j,  0.01-1.24j, -0.07+0.82j],
@@ -118,10 +114,14 @@ def normalize(X, Y):
 
 
 def modes_system_undamped(M, K):
-    """
-    This function will return the natural frequencies (w),
-    eigenvectors (P), mode shapes (S) abd the modal transformation
-    matrix S^-1(takes x -> r(modal coordinates) for an undamped system.
+    """Natural frequencies, mass-normalized mode shapes and eigenvectors of
+    MDOF system
+
+    Returns the natural frequencies (w),
+    eigenvectors (P), mode shapes (S) and the modal transformation
+    matrix S for an undamped system.
+
+    See Notes for explanation of the underlying math.
 
     Parameters
     ----------
@@ -137,9 +137,40 @@ def modes_system_undamped(M, K):
     P: array
         The eigenvectors of the system are.
     S: array
-        The mode shapes of the system.
+        The mass-normalized mode shapes of the system.
     Sinv: array
         The modal transformation matrix S^-1(takes x -> r(modal coordinates))
+
+    Notes
+    -----
+    Given :math:`M\ddot{x}(t)+Kx(t)=0`, with mode shapes :math:`u`, the matrix
+    of mode shapes :math:`S=[u_1 u_1 \ldots]` can be created. If the modal
+    coordinates are the vector :math:`r(t)`. The modal transformation separates
+    space and time from :math:`x(t)` such that :math:`x(t)=S r(t)`.
+    Substituting into the governing equation:
+
+    :math:`MS\ddot{r}(t)+KSr(t)=0`
+
+    Premultiplying by :math:`S^T`
+
+    :math:`S^TMS\ddot{r}(t)+S^TKSr(t)=0`
+
+    The matrices :math:`S^TMS` and :math:`S^TKS` will be diagonalized by this
+    process (:math:`u_i` are the eigenvectors of :math:`M^{-1}K`).
+
+    If scaled properly (mass normalized so :math:`u_i^TMu_i=1`) then
+    :math:`S^TMS=I` and :math:`S^TKS=\Omega^2` where :math:`\Omega^2` is a
+    diagonal matrix of the natural frequencies squared in radians per second.
+
+    Further, inverses are unstable so the better way to solve linear equations is with
+    Gauss elimination.
+
+    :math:`AB=C` given known :math:`A` and :math:`C`
+    is solved using `la.solve(A, C, assume_a='pos')`.
+
+    :math:`BA=C` given known :math:`A` and :math:`C` is solved by first
+    transposing the equation to :math:`A^TB^T=C^T`, then solving for
+    :math:`C^T`. The resulting command is `la.solve(A.T, C.T, assume_a='pos').T`
 
     Examples
     >>> M = np.array([[4, 0, 0],
@@ -151,15 +182,18 @@ def modes_system_undamped(M, K):
     >>> w, P, S, Sinv = modes_system_undamped(M, K)
     >>> w
     array([ 0.45,  1.25,  1.8 ])
-
+    >>> S
+    array([[ 0.16, -0.37, -0.3 ],
+       [ 0.3 , -0.16,  0.37],
+       [ 0.37,  0.3 , -0.16]])
     """
 
     L = la.cholesky(M)
-    Linv = la.inv(L)
-    lam, P = eigen(Linv @ K @ Linv.T)
+    lam, P = _eigen(la.solve(L, la.solve(L, K, assume_a='pos').T,
+                             assume_a='pos').T)
     w = np.real(np.sqrt(lam))
-    S = Linv @ P
-    Sinv = P.T @ Linv
+    S = la.solve(L, P, assume_a='pos')
+    Sinv = la.solve(L.T, P, assume_a='pos').T
 
     return w, P, S, Sinv
 
@@ -199,49 +233,51 @@ def modes_system(M, K, C=None):
     --------
     >>> M = np.array([[1, 0],
     ...               [0, 1]])
-    >>> K = np.array([[1, -0.4],
-    ...               [0.4, 6]])
-    >>> C = np.array([[0.3, -4],
-    ...               [4, 0.2]])
+    >>> K = np.array([[2, -1],
+    ...               [-1, 6]])
+    >>> C = np.array([[0.3, -0.02],
+    ...               [-0.02, 0.1]])
     >>> wn, wd, zeta, X, Y = modes_system(M, K, C)
     Damping is non-proportional, eigenvectors are complex.
     >>> wn
-    array([ 0.52,  4.77,  0.52,  4.77])
+    array([ 1.33,  2.5 ,  1.33,  2.5 ])
     >>> wd
-    array([ 0.51,  4.77, -0.51, -4.77])
+    array([ 1.32,  2.5 ,  1.32,  2.5 ])
     >>> zeta
-    array([ 0.22,  0.03,  0.22,  0.03])
+    array([ 0.11,  0.02,  0.11,  0.02])
     >>> X
-    array([[ 0.84+0.j  ,  0.14-0.j  ,  0.84-0.j  ,  0.14+0.j  ],
-           [ 0.01-0.3j ,  0.00+0.15j,  0.01+0.3j ,  0.00-0.15j],
-           [-0.09+0.42j, -0.01+0.65j, -0.09-0.42j, -0.01-0.65j],
-           [ 0.15+0.04j, -0.74+0.j  ,  0.15-0.04j, -0.74-0.j  ]])
+    array([[-0.06-0.58j, -0.01+0.08j, -0.06+0.58j, -0.01-0.08j],
+           [-0.00-0.14j, -0.01-0.36j, -0.00+0.14j, -0.01+0.36j],
+           [ 0.78+0.j  , -0.21-0.03j,  0.78-0.j  , -0.21+0.03j],
+           [ 0.18+0.01j,  0.90+0.j  ,  0.18-0.01j,  0.90-0.j  ]])
     >>> Y
-    array([[ 0.58-0.05j,  0.12-0.06j,  0.58+0.05j,  0.12+0.06j],
-           [ 0.01+1.26j, -0.06-0.83j,  0.01-1.26j, -0.06+0.83j],
-           [-0.00-0.3j ,  0.02-0.58j, -0.00+0.3j ,  0.02+0.58j],
-           [ 0.11+0.j  , -0.66-0.01j,  0.11-0.j  , -0.66+0.01j]])
-    >>> C = K*2 # with proportional damping
+    array([[ 0.02+0.82j,  0.01-0.31j,  0.02-0.82j,  0.01+0.31j],
+           [-0.05+0.18j,  0.01+1.31j, -0.05-0.18j,  0.01-1.31j],
+           [ 0.61+0.06j, -0.12-0.02j,  0.61-0.06j, -0.12+0.02j],
+           [ 0.14+0.03j,  0.53+0.j  ,  0.14-0.03j,  0.53-0.j  ]])
+    >>> C = 0.2*K # with proportional damping
     >>> wn, wd, zeta, X, Y = modes_system(M, K, C)
     Damping is proportional or zero, eigenvectors are real
     >>> X
-    array([[-1.  ,  0.08],
-           [ 0.08, -1.  ]])
+    array([[-0.97,  0.23],
+           [-0.23, -0.97]])
     """
 
     n = len(M)
 
     Z = np.zeros((n, n))
     I = np.eye(n)
-    Minv = la.inv(M)
 
     if (C is None or np.all(C == 0) or  # check if C has only zero entries
-            la.norm(Minv @ C @ K - Minv @ K @ C, 2) <
-            1e-8*la.norm(Minv @ K @ C, 2)):
+            la.norm(la.solve(M, C, assume_a='pos') @ K
+                    - la.solve(M, K, assume_a='pos') @ C, 2) <
+                    1e-8*la.norm(la.solve(M, K, assume_a='pos') @ C, 2)):
         w, P, S, Sinv = modes_system_undamped(M, K)
         wn = w
         wd = w
-        zeta = None
+        #zeta = None
+        zeta = np.diag(S.T@C@S)/2/wn
+        wd = wn*np.sqrt(1-zeta**2)
         X = P
         Y = P
         print('Damping is proportional or zero, eigenvectors are real')
@@ -252,16 +288,18 @@ def modes_system(M, K, C=None):
 
     # creates the state space matrix
     A = np.vstack([np.hstack([Z, I]),
-                   np.hstack([-la.pinv(M) @ K, -la.pinv(M) @ C])])
+                   np.hstack([-la.solve(M, K, assume_a='pos'),
+                              -la.solve(M, C, assume_a='pos')])])
 
-    w, X = eigen(A)
-    _, Y = eigen(A.T)
+    w, X = _eigen(A)
+    _, Y = _eigen(A.T)
 
-    wd = np.imag(w)
+    wd = abs(np.imag(w))
     wn = np.absolute(w)
     zeta = (-np.real(w)/np.absolute(w))
 
-    Y = normalize(X, Y)
+
+    Y = _normalize(X, Y)
 
     print('Damping is non-proportional, eigenvectors are complex.')
 
@@ -322,7 +360,7 @@ def response_system_undamped(M, K, x0, v0, max_time):
 
     # creates the state space matrix
     A = np.vstack([np.hstack([Z,               I]),
-                   np.hstack([-la.pinv(M) @ K, Z])])
+                   np.hstack([-la.solve(M, K, assume_a='pos'), Z])])
 
     # creates the x array and set the first line according to the initial
     # conditions
@@ -419,7 +457,8 @@ def response_system(M, C, K, F, x0, v0, t):
 
     # creates the state space matrix
     A = np.vstack([np.hstack([Z,               I]),
-                   np.hstack([-la.pinv(M) @ K, -la.pinv(M) @ C])])
+                   np.hstack([-la.solve(M, K, assume_a='pos'),
+                              -la.solve(M, C, assume_a='pos')])])
     B = np.vstack([Z,
                    la.inv(M)])
     C = np.eye(2*n)
@@ -432,21 +471,3 @@ def response_system(M, C, K, F, x0, v0, t):
     T, yout, xout = signal.lsim(sys, F, t, IC)
 
     return T, yout, xout
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod(optionflags=doctest.ELLIPSIS)
-    # doctest.run_docstring_examples(frfest,globals(),optionflags=doctest.ELLIPSIS)
-    # doctest.run_docstring_examples(asd,globals(),optionflags=doctest.ELLIPSIS)
-    """ What this does.
-    python (name of this file)  -v
-    will test all of the examples in the help.
-
-    Leaving off -v will run the tests without any output. Success will return
-    nothing.
-
-    See the doctest section of the python manual.
-    https://docs.python.org/3.5/library/doctest.html
-    """
