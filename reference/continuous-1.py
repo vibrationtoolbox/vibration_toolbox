5e3152e3f54ab541996a3bc3db7c303497f2a54e
Joseph C. Slater <joseph.c.slater@gmail.com>
2019-07-28 07:41:06 -0600

    Generated gh-pages for commit 0539d9d Author: Joseph C. Slater <joseph.c.slater@gmail.com>
        Keep examples in sync

--- reference/continuous-1.py
+++ reference/continuous-1.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import vibration_toolbox as vtb
 omega_n, x, U = vtb.euler_beam_modes(n=1)
 plt.figure()
-# <matplotlib.figure...>
+# <Figure size 1000x600 with 0 Axes>
 plt.plot(x,U)
 # [<matplotlib.lines.Line2D object at ...>]
 plt.xlabel('x (m)')
-# Text(0.5,0,'x (m)')
+# Text(0.5, 0, 'x (m)')
 plt.ylabel('Displacement (m)')
-# Text(0,0.5,'Displacement (m)')
+# Text(0, 0.5, 'Displacement (m)')
 plt.title('Mode 1')
-# Text(0.5,1,'Mode 1')
-plt.grid('on')
+# Text(0.5, 1.0, 'Mode 1')
+plt.grid(True)
--- reference/sdof-1.py
+++ reference/sdof-1.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 import vibration_toolbox as vtb
-vtb.free_response()[1][:5] # get the first five values of x
-# array([[ 1.  ],
-# [ 1.  ],
-# [ 0.99],
-# [ 0.99],
-# [ 0.98]])
+vtb.free_response()[1][:5] # doctest: +SKIP
+# array([[1.  ],
+# [1.  ],
+# [0.99],
+# [0.99],
+# [0.98]])
 
 t, x, *_ = vtb.free_response() # *_ ignores all other returns
 plt.plot(t,x)
 # [<matplotlib.lines.Line2D object at ...>]
 plt.xlabel('Time (sec)')
-# Text(0.5,0,'Time (sec)')
+# Text(0.5, 0, 'Time (sec)')
 plt.ylabel('Displacement (m)')
-# Text(0,0.5,'Displacement (m)')
+# Text(0, 0.5, 'Displacement (m)')
 plt.title('Displacement versus time')
-# Text(0.5,1,'Displacement versus time')
-plt.grid('on')
+# Text(0.5, 1.0, 'Displacement versus time')
+plt.grid(True)
